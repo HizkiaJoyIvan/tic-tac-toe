628c6bd971d4fdd8050fc83fcdb7849563326d6c
Ivanosky <joyivan6@gmail.com>
2023-07-04 23:31:56 +0700

    Second commit

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 const board = document.querySelector('.game-container') as HTMLElement
 
 function main(): void {
-    makeBox()
+    createBoard()
 }
 
-function makeBox(): void {
+function createBoard(): void {
+    for(let i=0;i<9;i++) makeBox(i)
+}
+
+function makeBox(i: number): void {
     const box : HTMLDivElement = document.createElement("div")
     box.className = "box"
+    box.id = `box-${i}`
     box.textContent = "X"
     board.append(box)
 }
